d48528e838a32848cf22b301c6180b428cec8fad
guillotin <guillotin@LAPTOP-D60M1PQQ.localdomain>
2021-04-29 13:32:09 +0200

    First of the web part

--- web/app.py
+++ web/app.py
@@ -6,9 +6,9 @@ app = Flask(__name__)
 def index():
     return "Hello world !"
 
-@app.route('/welcome')
-def welcome() :
-	return render_template("welcome.html")
+@app.route('/index')
+def main() :
+	return render_template("index.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
