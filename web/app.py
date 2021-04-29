90bce639c677ddb5e7d8c75b90e1299feb0c52b3
REVEL Guillaume <grevel@ecole.ensicaen.fr>
2021-04-29 13:36:30 +0200

    fix warnings

--- web/app.py
+++ web/app.py
@@ -2,13 +2,16 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def index():
     return "Hello world !"
 
+
 @app.route('/welcome')
-def welcome() :
-	return render_template("welcome.html")
+def welcome():
+    return render_template("index.html")
+
 
 if __name__ == "__main__":
     app.run(debug=True)
