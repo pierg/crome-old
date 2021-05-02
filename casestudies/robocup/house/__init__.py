106a88ebae4b6397fc9809d8748b79023e62f6ed
Piergiuseppe Mallozzi <piergiuseppe.mallozzi@gmail.com>
2021-05-02 19:22:40 +0200

    refactoring

---
+++ casestudies/robocup/house/__init__.py
@@ -0,0 +1,32 @@
+from world import World
+from .types import *
+
+
+class RobocupHome(World):
+
+    def __init__(self):
+        super().__init__({
+            L1(),
+            L2(),
+            L3(),
+            L4(),
+            L5(),
+            L6(),
+            B1(),
+            B2(),
+            B3(),
+            H1(),
+            H2(),
+            E1(),
+            E2(),
+            K1(),
+            K2(),
+            K3(),
+            R1(),
+            R2(),
+            ObjectRecognized(),
+            ObjectUnknown(),
+            HoldObject(),
+            DropObject(),
+            PickupObject()
+        })
