384ea41d25fa4e5079a97d6e9394ec5673b66da9
alexg <alexandre.guillotin@ecole.ensicaen.fr>
2021-05-05 17:02:11 +0200

    Fix conflicts

--- web/flask-backend/main.py
+++ web/flask-backend/main.py
@@ -28,4 +28,4 @@ def run_example():
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 80))
-    app.run(host='0.0.0.0', port=port, debug=True)
+    app.run(host='127.0.0.1', port=port, debug=True)
