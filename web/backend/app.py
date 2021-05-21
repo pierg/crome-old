2cf49c43d36fe4e02d0184f06c2f2af910b6e045
Piergiuseppe Mallozzi <piergiuseppe.mallozzi@gmail.com>
2021-05-21 09:10:36 +0200

    refactoring

---
+++ web/backend/app.py
@@ -0,0 +1,36 @@
+from flask import Flask, Blueprint, request, jsonify
+from flask_cors import CORS
+
+app = Flask(__name__, static_folder='../frontend/build', static_url_path='/')
+CORS(app)
+api = Blueprint('api', __name__)
+
+
+@app.route('/')
+def index():
+    return app.send_static_file('index.html')
+
+
+@api.route('/submit', methods=['POST'])
+def handle_submit():
+    if request.method == "POST":
+        first_name = request.form['firstName']
+        last_name = request.form['lastName']
+        job = request.form['job']
+        print(f'first name : {first_name}')
+        print(f'last name : {last_name}')
+        print(f'job : {job}')
+
+        # do your processing logic here.
+
+        return jsonify({
+            "firstName": first_name,
+            "lastName": last_name,
+            "job": job
+        })
+
+
+app.register_blueprint(api, url_prefix='/api')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', debug=True, port=5050)
