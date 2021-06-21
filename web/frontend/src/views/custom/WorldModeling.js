4263c3e36a5b05eada16a95aef48ae4f8b8dcd92
REVEL Guillaume <grevel@ecole.ensicaen.fr>
2021-06-21 18:09:14 +0200

    WorldModeling: Basic Start

--- web/frontend/src/views/custom/WorldModeling.js
+++ web/frontend/src/views/custom/WorldModeling.js
@@ -1,24 +1,67 @@
 import "@fortawesome/fontawesome-free/css/all.min.css";
 import "../../assets/styles/tailwind.css";
-import SocketIoMessage from "../../components/Custom/Examples/SocketIoMessage";
-import FetchTime from "../../components/Custom/Examples/FetchTime";
-import SocketIoPatterns from "../../components/Custom/Examples/GetPatterns";
+import React from "react";
+import AddGoal from "../../components/Custom/AddGoal";
+import WorldView from "../../components/Custom/WorldView";
 
 
-function WorldModeling() {
+export default class WorldModeling extends React.Component {
 
-    return (
-        <>
-            <FetchTime/>
-            <SocketIoMessage/>
-            <SocketIoPatterns/>
-            {/*<SocketIoGaols/>*/}
-        </>
-    );
+    state = {
+        worlds: [],
+        currentGoalIndex: 0,
+        numChildren: 0,
+    }
+
+    render() {
+
+        const children = [];
+        for (let i = 0; i < this.state.numChildren; i += 1) {
+            children.push(<WorldView key={i} number={i}
+                                    title={this.state.worlds[i].name}
+                                    description={this.state.worlds[i].description}
+                                    statIconName={this.props.info.goalComponent.editIconName}
+                                    statSecondIconName={this.props.info.goalComponent.deleteIconName}
+                                    statIconColor={this.props.info.goalComponent.iconColor}
+                                    modify={this.setModalClassic}
+                                    delete={this.deleteGoal}
+            />);
+        }
+
+        return (
+            <>
+                {/*<SocketIoWorlds worlds={this.getWorlds} />*/}
+                <ParentComponent build={this.redirectToBuilding}>
+                    {children}
+                </ParentComponent>
+            </>
+        );
+    }
+
+    redirectToBuilding = () => {
+        console.log("redirection")
+    }
+
 }
 
-export default WorldModeling;
-
+const ParentComponent = props => (
+    <section className="mt-5 mt-xl-2 pt-2 relative">
+        <div className="px-4 md:px-10 mx-auto w-full">
+            <div>
+                <div className="flex justify-center">
+                    <div onClick={props.build} className="w-full lg:w-6/12 xl:w-3/12 mt-8 ml-4 mr-4 px-4 relative flex flex-col min-w-0 break-words bg-lightBlue-600 rounded mb-6 xl:mb-0 shadow-lg opacity-1 transform duration-300 transition-all ease-in-out">
+                        <AddGoal
+                            statText="Build your Environment"
+                            statIconName="fas fa-plus-square"
+                            statIconColor="text-lightBlue-700"
+                        /></div></div>
+                <div className="flex flex-wrap justify-center">
+                    {props.children}
+                </div>
+            </div>
+        </div>
+    </section>
+);
 
 //
 // export default class WorldModeling extends React.Component {
