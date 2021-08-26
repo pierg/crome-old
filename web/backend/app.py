07b745e84ac330edbad7fd4bd4d8a2ec66ba824c
pier <piergiuseppe.mallozzi@gmail.com>
2021-08-26 19:01:58 +0200

    test

--- web/backend/app.py
+++ web/backend/app.py
@@ -30,13 +30,13 @@ if storage_folder.exists():
     print(storage_folder)
 
 
-if build_folder.exists():
-    print("serving math as well")
-    app = Flask(__name__, static_folder=str(build_folder), static_url_path='/')
-else:
-    app = Flask(__name__)
+# if build_folder.exists():
+#     print("serving math as well")
+#     app = Flask(__name__, static_folder=str(build_folder), static_url_path='/')
+# else:
+#     app = Flask(__name__)
 
-# app = Flask(__name__)
+app = Flask(__name__)
 
 socketio = SocketIO(app, cors_allowed_origins='*')
 
