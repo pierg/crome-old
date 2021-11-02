788b2f3082e154a4d50af8648f55961e79aee261
pier <piergiuseppe.mallozzi@gmail.com>
2021-11-02 16:32:12 -0700

    refactoring

--- src/examples/contract operations/casestudy/separation.py
+++ src/examples/contract operations/casestudy/separation.py
@@ -4,7 +4,7 @@ from custom_world import CustomWorld
 
 w = CustomWorld()
 
-original_spec = Node(name="strict_patrolling",
+original = Node(name="strict_patrolling",
                      description="strict order patrolling regions r1 -> r2",
                      specification=StrictOrderedPatrolling([w["r1"], w["r2"], w["r5"]]),
                      world=w)
@@ -13,9 +13,9 @@ original_spec = Node(name="strict_patrolling",
     G: G(F(r1 & F(r2))) & (!(r2) U r1) & G(((r2) -> (X((!(r2) U r1))))) & G(((r1) -> (X((!(r1) U r2))))) 
 """
 
-print(original_spec)
+print(original)
 
-print("let us now try to refine 'strict_order' from 'library_1'")
+print("let us now try to refine 'original_goal' from 'library_1'")
 
 library_1_goal_z = Node(name="library_1_goal_z",
                         description="strict order patrolling regions r1 -> r2",
@@ -23,34 +23,34 @@ library_1_goal_z = Node(name="library_1_goal_z",
                         world=w)
 
 print("we found a goal in library 1 which has some common variables but does not refine 'strict_patrolling'")
-assert set(library_1_goal_z.specification.typeset).intersection(original_spec.specification.typeset)
-assert not library_1_goal_z.specification <= original_spec.specification
+assert set(library_1_goal_z.specification.typeset).intersection(original.specification.typeset)
+assert not library_1_goal_z.specification <= original.specification
 
 print("let's use the 'QUOTIENT' to look at the missing part")
-quotient_original_spec = original_spec.quotient(library_1_goal_z)
-print(quotient_original_spec)
+quotient_original = original.quotient(library_1_goal_z)
+print(quotient_original)
 
 print("let's look for something that refine the quotient in another library")
 print("unfortunately we could not find anything that refine the quotient")
 print("let's compute the separation on what we have a find the smallest change we have to add to our specification "
       "so that the goal we have is enough to refine")
 
-separation_1 = library_1_goal_z.separation(original_spec)
+separation_1 = library_1_goal_z.separation(original)
 print(separation_1)
 
 print("now we can merge what we found with our original goal")
-merger = Node.merging({original_spec, separation_1})
+merger = Node.merging({original, separation_1})
 print(merger)
 print("our library goal now refines the merged specification")
 assert library_1_goal_z.specification <= merger.specification
 
 print("thanks to the separation and the merging the initial specification has been 'relaxed' "
       "to accommodate what is available in the library")
-assert original_spec.specification <= merger.specification
+assert original.specification <= merger.specification
 
-print("we can go back to the original 'strict_order' specification my separating "
+print("we can go back to the original 'original_goal' specification my separating "
       "the merger from the previous separation result")
 
 separation_2 = merger.separation(separation_1)
 
-assert separation_2.specification == original_spec.specification
+assert separation_2.specification == original.specification
